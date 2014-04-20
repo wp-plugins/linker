b5d14166af3f5a6e465809c3b6685d10040f2eec
KingYes <KingYes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-20 12:41:30 +0000

    test
    git-svn-id: https://plugins.svn.wordpress.org/linker/trunk@898628 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/class-linker-cpt.php
@@ -0,0 +1,191 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+class Linker_CPT {
+
+	public function register_post_type() {
+		$labels = array(
+			'name'               => __( 'Linker', 'linker' ),
+			'singular_name'      => __( 'Link', 'linker' ),
+			'add_new'            => __( 'Add New', 'linker' ),
+			'add_new_item'       => __( 'Add New Link', 'linker' ),
+			'edit'               => __( 'Edit', 'linker' ),
+			'edit_item'          => __( 'Edit Link', 'linker' ),
+			'new_item'           => __( 'New Link', 'linker' ),
+			'view'               => __( 'View Link', 'linker' ),
+			'view_item'          => __( 'View Link', 'linker' ),
+			'search_items'       => __( 'Search Link', 'linker' ),
+			'not_found'          => __( 'No Links found', 'linker' ),
+			'not_found_in_trash' => __( 'No Links found in Trash', 'linker' ),
+		);
+
+		$args = array(
+			'labels'          => $labels,
+			'public'          => true,
+			'query_var'       => true,
+			'capability_type' => 'post',
+			'has_archive'     => false,
+			'hierarchical'    => false,
+			'menu_position'   => 30,
+			'supports'        => array( 'title', 'author' ),
+			'rewrite'         => array(
+				'slug'       => apply_filters( 'linker_prefix_slug', 'go' ),
+				'with_front' => false
+			),
+		);
+		
+		register_post_type( 'linker',
+			apply_filters( 'linker_register_post_type_args', $args )
+		);
+	}
+
+	public function post_updated_messages( $messages ) {
+		global $post;
+
+		$messages['linker'] = array(
+			0  => '', // Unused. Messages start at index 1.
+			1  => __( 'Link updated.', 'linker' ),
+			2  => __( 'Custom field updated.', 'linker' ),
+			3  => __( 'Custom field deleted.', 'linker' ),
+			4  => __( 'Link updated.', 'linker' ),
+			/* translators: %s: date and time of the revision */
+			5  => isset( $_GET['revision'] ) ? sprintf( __( 'Link restored to revision from %s', 'linker' ), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+			6  => __( 'Link published.', 'linker' ),
+			7  => __( 'Link saved.', 'linker' ),
+			8  => __( 'Link submitted.', 'linker' ),
+			9  => sprintf( __( 'Post scheduled for: <strong>%1$s</strong>.', 'linker' ),
+				// translators: Publish box date format, see http://php.net/date
+				date_i18n( __( 'M j, Y @ G:i', 'linker' ), strtotime( $post->post_date ) ) ),
+			10 => __( 'Link draft updated.', 'linker' ),
+		);
+
+		return $messages;
+	}
+
+	public function plugin_action_links( $links ) {
+		$settings_link = sprintf( '<a href="%s" target="_blank">%s</a>', 'https://github.com/KingYes/wp-linker', __( 'GitHub', 'linker' ) );
+		array_unshift( $links, $settings_link );
+
+		return $links;
+	}
+
+	public function admin_cpt_columns( $columns ) {
+		return array(
+			'cb'               => '<input type="checkbox" />',
+			'title'            => __( 'Title', 'linker' ),
+			'linker_url'       => __( 'Redirect to', 'linker' ),
+			'linker_permalink' => __( 'Permalink', 'linker' ),
+			'linker_clicks'    => __( 'Clicks', 'linker' ),
+			'author'           => __( 'Author', 'linker' ),
+			'date'             => __( 'Date', 'linker' ),
+		);
+	}
+
+	public function custom_columns( $column ) {
+		global $post;
+
+		switch ( $column ) {
+			case 'linker_url' :
+				echo make_clickable( get_post_meta( $post->ID, '_linker_redirect', true ) );
+				break;
+			
+			case 'linker_permalink' :
+				echo make_clickable( get_permalink( $post->ID ) );
+				break;
+			
+			case 'linker_clicks' :
+				echo absint( get_post_meta( $post->ID, '_linker_count', true ) );
+				break;
+		}
+	}
+
+	public function register_meta_box() {
+		add_meta_box(
+			'linker-url-information',
+			__( 'Link Info', 'linker' ),
+			array( &$this, 'render_meta_box' ),
+			'linker',
+			'normal',
+			'high'
+		);
+	}
+
+	public function admin_header() {
+		// TODO: move to a separate file.
+		?><style>
+			#adminmenu #menu-posts-linker div.wp-menu-image:before {
+				content: "\f103";
+			}
+			.fixed .column-linker_clicks {
+				width: 10%;
+			}
+		</style>
+	<?php
+	}
+
+	public function render_meta_box( $post ) {
+		wp_nonce_field( basename( __FILE__ ), '_linker_meta_box_nonce' );
+		
+		$field_id = '_linker_redirect';
+		echo strtr( '<p><strong><label for="{name}">{label}</label></strong></p><p><input type="url" id="{name}" name="{name}" value="{value}" placeholder="{placeholder}" class="large-text" /></p>', array(
+			'{label}' => __( 'Redirect Link:', 'linker' ),
+			'{name}'  => $field_id,
+			'{placeholder}' => __( 'http://your-link.com/', 'linker' ),
+			'{value}' => esc_attr( get_post_meta( $post->ID, $field_id, true ) ),
+		) );
+
+		$counter = absint( get_post_meta( $post->ID, '_linker_count', true ) );
+		printf( '<p class="description">' . __( 'This Link has been accessed <strong>%d</strong> times.', 'linker' ) . '</p>', $counter );
+	}
+
+	public function save_post( $post_id ) {
+		if ( ! isset( $_POST['_linker_meta_box_nonce'] ) || ! wp_verify_nonce( $_POST['_linker_meta_box_nonce'], basename( __FILE__ ) ) )
+			return;
+
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+			return;
+
+		if ( defined( 'DOING_AJAX' ) && DOING_AJAX )
+			return;
+
+		if ( defined( 'DOING_CRON' ) && DOING_CRON )
+			return;
+		
+		if ( isset( $_POST['_linker_redirect'] ) )
+			update_post_meta( $post_id, '_linker_redirect', $_POST['_linker_redirect'] );
+		else
+			delete_post_meta( $post_id, '_linker_redirect' );
+	}
+
+	public function count_and_redirect() {
+		if ( ! is_singular( 'linker' ) )
+			return;
+
+		$counter = absint( get_post_meta( get_the_ID(), '_linker_count', true ) );
+		update_post_meta( get_the_ID(), '_linker_count', ++$counter );
+
+		$redirect_url = esc_url_raw( get_post_meta( get_the_ID(), '_linker_redirect', true ) );
+		
+		if ( ! empty( $redirect_url ) )
+			wp_redirect( $redirect_url, 301 );
+		else
+			wp_redirect( home_url(), 302 );
+		
+		die();
+	}
+	
+	public function __construct() {
+		// TODO: please add updated messages
+		
+		add_action( 'init', array( &$this, 'register_post_type' ) );
+		add_action( 'admin_menu', array( &$this, 'register_meta_box' ) );
+		add_action( 'admin_head', array( &$this, 'admin_header' ) );
+		add_filter( 'plugin_action_links_' . LINKER_BASE, array( &$this, 'plugin_action_links' ) );
+		
+		add_filter( 'manage_edit-linker_columns', array( &$this, 'admin_cpt_columns' ) );
+		add_action( 'manage_posts_custom_column', array( &$this, 'custom_columns' ) );
+		add_action( 'save_post', array( &$this, 'save_post' ) );
+		add_action( 'template_redirect', array( &$this, 'count_and_redirect' ) );
+	}
+	
+}
---
+++ readme.txt
@@ -0,0 +1,63 @@
+=== Linker ===
+Contributors: KingYes, ariel.k
+Tags: click tracking, custom post types, redirect, 301, outbound links, pretty links, link manager, links, google analytics, affiliates, external links,
+Requires at least: 3.5
+Tested up to: 3.9
+Stable tag: 1.0.0
+License: GPLv2 or later
+
+Manage, create and track outbound links by custom pretty links with your domain. e.g. your-domain.com/go/fb-link
+
+== Description ==
+
+Linker is a simple & easy plugin to manage, create and track outbound links from your website. Linker adds a new custom post type to your Admin menu, where you can create, edit, delete, and manage Links by 301 redirects.
+
+Linker is tracking click counts and save them of a custom field on that custom post type, it really simple & cool. 
+
+<h4>Integration with Google Analytics</h4>
+You can track with Google Analytics to get full statistics by Event Tracking.
+
+ This plagin work well too with [Google Analytics for WordPress](http://wordpress.org/plugins/google-analytics-for-wordpress/) by Yoast. 
+
+Go to the Setting Page of Google Analytics under the Setting Menu, and go to > Internal Links to Track as Outbound  > enter "/go/" in the field > choose Label to use.
+
+Now you can show up in Analytics all clicks out. go to Content > Event Tracking > Categories, and you’ll see your Label that used. that's all.
+
+<h4>Thanks</h4>
+We took inspiration to our plugin from [Simple URL](http://wordpress.org/plugins/simple-urls/) plugin - so thank you so much, you done good job.
+
+<strong>Contributions:</strong><br />
+
+Would you like to like to contribute to Linker? You are more than welcome to submit your pull requests on the [GitHub repo](https://github.com/KingYes/wp-linker). Also, if you have any notes about the code, please open a ticket on ths issue tracker.
+
+
+== Installation ==
+
+1. Upload plugin files to your plugins folder or install using WordPress built-in Add New Plugin installer
+1. Activate the plugin
+1. Navigate to Settings > Permalinks and save them. Yes, just click Save Changes.
+1. Go to the Linker Menu under the Pages
+1. Create a new Link and publish
+1. Now you can track how many times clicked on each link
+
+== Screenshots ==
+
+1. New Link
+2. All Links
+
+== Frequently Asked Questions ==
+
+= Can I change the Link structure other than /go/ ? =
+* Sure. Just use with `linker_prefix_slug` filter.
+
+= Requirements =
+* __Requires PHP5__ for list management functionality.
+
+= What is the plugin license? =
+* This plugin is released under a GPL license.
+
+
+== Changelog ==
+
+= 1.0.0 =
+* Blastoff!
